ae0252e63d41afa86a45bc6bc55c011ae4498203
Samuel Jones <sjones.nova@gmail.com>
2026-08-31 16:27:18 -0400

    postcondition unit tests

--- tests/test_boolvalidator.py
+++ tests/test_boolvalidator.py
@@ -14,6 +14,17 @@ def precondition_sqrt():
     )(sqrt)
 
 
+@pytest.fixture
+def postcondition_buggy_absolute():
+    def buggy_absolute(x: int) -> int:
+        return x
+
+    return BoolValidator.post(
+        lambda r: r >= 0,  # type: ignore
+        lambda r: ValueError(f"Expected value greater than 0. Got {r}"),
+    )(buggy_absolute)
+
+
 class TestBoolValidator:
     def test_precondition_passes(self, precondition_sqrt):
         precondition_sqrt(1.0)
@@ -21,5 +32,14 @@ class TestBoolValidator:
     def test_precondition_fails(self, precondition_sqrt):
         with pytest.raises(ValueError) as exc:
             precondition_sqrt(-1.0)
+
+            exc.match("Expected value greater than 0.")
+
+    def test_postcondition_passes(self, postcondition_buggy_absolute):
+        postcondition_buggy_absolute(1)
+
+    def test_postcondition_fails(self, postcondition_buggy_absolute):
+        with pytest.raises(ValueError) as exc:
+            postcondition_buggy_absolute(-1.0)
 
             exc.match("Expected value greater than 0.")
