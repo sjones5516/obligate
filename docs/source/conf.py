685e42c30198f47e33ff67cac9eaa5e8ef94fe82
Samuel Jones <sjones.nova@gmail.com>
2026-09-01 16:50:15 -0400

    add autodoc extension and src

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,33 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+from pathlib import Path
+
+# Add src directory to Python path
+sys.path.insert(0, str(Path(__file__).parent.parent.parent / "src"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Obligate'
-copyright = '2026, Samuel Jones'
-author = 'Samuel Jones'
-release = '2.0.0'
+project = "Obligate"
+copyright = "2026, Samuel Jones"
+author = "Samuel Jones"
+release = "2.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
